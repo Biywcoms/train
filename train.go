8a334735f6a2a29d32b1d6548c547c6790c01a44
Guan <Guan@mail.com>
2019-04-02 17:00:53 +0800

    addTwoNumbers v1

--- train.go
+++ train.go
@@ -12,35 +12,38 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	i := 1
-	sum := 0
+	var l3 *ListNode
+	i := 10
+	j := 0
 	for l1 != nil && l2 != nil {
-		sum += l1.Val*i + l2.Val*i
-		i = i * 10
+		sum := l1.Val + l2.Val
+		tmp := ListNode{sum%i + j, nil}
+		j = sum / i
+		if l3 == nil {
+			l3 = &tmp
+			fmt.Println(0, l3)
+		} else {
+			tmp.Next = l3
+			l3 = &tmp
+			fmt.Println(1, l3)
+		}
 		l1 = l1.Next
 		l2 = l2.Next
 	}
-
-	for i > 1 {
-		d := i
-		i = i / 10
-		fmt.Println(i, d, (sum/i)%d)
-	}
-
-	return &ListNode{sum, nil}
+	return l3
 }
 
 func main() {
 	t := time.Now()
+
 	l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
 	l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 	result := addTwoNumbers(&l1, &l2)
-	fmt.Println(result)
-	fmt.Println(903 / 10)
-	//	for i:=0;i<3;i++{
-	//		fmt.Println(result)
-	//		result = result.Next
-	//	}
+	for result != nil {
+		fmt.Println(result)
+		result = result.Next
+	}
+
 	fmt.Println("Timing:", time.Since(t))
 
 }
