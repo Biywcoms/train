28be160d13ffb0b7e2951aa06f0b658723aa2368
Guan <guan@mail.com>
2019-11-01 18:29:56 +0800

    update

--- clojure/clojure-noob/src/clojure_noob/core.clj
+++ clojure/clojure-noob/src/clojure_noob/core.clj
@@ -448,3 +448,25 @@
 (concat '(1 2) '(1 4))
 (conj [1 2] [1 4])
 (conj '(1 2) '(1 4))
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
